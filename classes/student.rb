2c3b273beef7f1330bf23269bb3d9d6f87d9aa6b
Robertson Arthur <robertsonakpan@gmail.com>
2022-08-25 17:01:34 +0100

    Add public method _add_to_classroom_ to student class

--- classes/student.rb
+++ classes/student.rb
@@ -11,4 +11,10 @@ class Student < Person
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  def add_to_classroom(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
+  end
+
 end
