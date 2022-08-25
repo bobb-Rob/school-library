0986c86244f5e5bd921fb4dd182c5dee726bbd1f
Robertson Arthur <robertsonakpan@gmail.com>
2022-08-25 16:35:23 +0100

    Update person class with @rentals instance variable

--- classes/person.rb
+++ classes/person.rb
@@ -3,6 +3,8 @@ require_relative 'nameable'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 require_relative 'base_decorator'
+require_relative 'rental'
+require_relative 'book'
 
 class Person < Nameable
   def initialize(age, name, parent_permission: true)
@@ -10,11 +12,12 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
 
   attr_reader :id
-  attr_accessor(:name, :age)
+  attr_accessor(:name, :age, :rentals)
 
   def correct_name
     @name
@@ -30,11 +33,3 @@ class Person < Nameable
     @age >= 18
   end
 end
-
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
